8526fd38ae33d7ffbb25d3722c1b01c301297d3e
scienceappreciation <colehale059@gmail.com>
2023-09-20 08:03:49 -0500

    Created Basic App Structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import SearchBar from './components/SearchBar';
+import SearchResults from './components/SearchResults';
+import Playlist from './components/Playlist';
+import Tracklist from './components/Tracklist';
 
 function App() {
   return (
     <>
       <h1>Jammming</h1>
       <span>An App for all of your playlist needs.</span>
+      <SearchBar />
+      <SearchResults />
+      <Playlist />
+      <Tracklist />
     </>
   );
 }
