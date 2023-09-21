10625275f68b2b823f63b94b74a17f1a8d60c54e
scienceappreciation <colehale059@gmail.com>
2023-09-21 11:03:14 -0500

    Token Collection Logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from './components/SearchBar';
 import SearchResults from './components/SearchResults';
 import Playlist from './components/Playlist';
 import TrackData from './classes/TrackData';
 import mainStyle from './App.module.css';
+import './modules/Authorization/Authorization';
+import { buildRequestUrl, formatParamsAsObject } from './modules/Authorization/Authorization';
+
+import SpotifyLoginButton from './components/SpotifyLoginButton';
 
 /* TODO: App CSS */
 /* TODO: Search CSS */
@@ -25,6 +29,36 @@ function App() {
   const [ searchResults, setSearchResults ] = useState(null);
   const [ playListTitle, setPlayListTitle ] = useState('');
   const [ trackList, setTrackList ] = useState([]);
+  const [ loggedIn, setLoggedIn ] = useState(false);
+  const [ accessToken, setAccessToken ] = useState('');
+
+  function validateAccess() {
+    const urlParams = formatParamsAsObject(window.location.origin, window.location.href);
+
+    const refreshUrl = () => window.location.href = window.location.origin;
+    const logout = () => setLoggedIn(() => false);
+    const clearToken = () => setAccessToken(() => '');
+    const resetSession = () => { refreshUrl(); logout(); clearToken(); }
+
+    if (urlParams["error"] && urlParams["error"] === "access_denied") {
+      resetSession();
+      return;
+    }
+
+    if (!urlParams['state']) return;
+
+    // Since this point is unreachable otherwise, the following code assumes a valid session.
+    const access_token = urlParams["access_token"];
+    const expires_in_ms = urlParams["expires_in"] * 1000;
+    setLoggedIn(() => true);
+    setAccessToken(() => access_token);
+
+    setTimeout(() => {resetSession()}, expires_in_ms);
+  }
+
+  useEffect(() => {
+    validateAccess();
+  }, []);
 
   function handleSearch(e) {
     e.preventDefault();
@@ -49,6 +83,14 @@ function App() {
     setTrackList((prev) => {
       return prev.filter(track => !TrackData.compare(track, data));
     });
+  }
+
+  if (!loggedIn) {
+    return (<div>
+      <h1 className={mainStyle["page-title"]}>Jammming</h1>
+      <h2 className={mainStyle["page-subtitle"]}>An App for all of your playlist needs.</h2>
+      <SpotifyLoginButton url={buildRequestUrl()} />
+    </div>);
   }
 
   return (
